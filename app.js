7089baf66fc7c7c2dd20b9000935b3a53c3c452c
Discoverrizwan <rizwanhz988@gmail.com>
2024-03-23 14:46:05 +0500

    number guessing game

---
+++ app.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'game',
+        message: 'Guess a rendom number 1 or 5 for play a game'
+    }
+]);
+const comptuerChoise = Math.floor(Math.random() * 5) + 1;
+if (answer.game === comptuerChoise) {
+    console.log(`Wow you guess correct number`);
+}
+else {
+    console.log(`You guess wrong number`);
+}
